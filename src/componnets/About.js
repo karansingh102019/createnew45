9318cffceda0186d4e938f88803d7b6504da67d9
karan singh negi <kn8610519@gmail.com>
2022-12-27 16:40:59 +0530

    move project online

---
+++ src/componnets/About.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+
+function Myabout()
+{
+    return (
+        <Fragment>
+         <h1>Hello...... guys this my first react page</h1>
+            <h2>Hello ...... and this is my 2 code</h2>
+        </Fragment>
+
+    ) 
+}
+
+export default Myabout
---
+++ src/componnets/Contract.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Abc extends React.Component
+{
+    render()
+    {
+        return 300
+    }
+}
+
+export default Abc
